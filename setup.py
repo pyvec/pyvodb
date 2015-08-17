662c48bebe26c8c667827b2dfa23c29b093a39f3
Petr Viktorin <encukou@gmail.com>
2015-08-17 18:23:03 +0200

    Bump up the version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ class PyTest(TestCommand):
 
 setup_args = dict(
     name='pyvodb',
-    version='0.0.1',
+    version='0.0.2',
     packages=['pyvodb'],
     url='https://github.com/pyvec/pyvodb',
 
