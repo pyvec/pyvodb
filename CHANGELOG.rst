f6ac006ad678ee8432c5021b2b7af57944531d98
Petr Viktorin <encukou@gmail.com>
2015-09-06 21:28:39 +0200

    Bump version to 0.2

---
+++ CHANGELOG.rst
@@ -0,0 +1,9 @@
+## 0.2 (2015-09-06)
+
+* Recognize `https` Youtube links
+* Support Python 3.3
+
+## 0.1
+
+* Initial release
+
--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ if sys.version_info < (3, 4):
 
 setup_args = dict(
     name='pyvodb',
-    version='0.1',
+    version='0.2',
     packages=['pyvodb'],
     url='https://github.com/pyvec/pyvodb',
 
