e9850879423f42f5c8e550ae3c89914a2e6577db
Petr Viktorin <encukou@gmail.com>
2017-02-03 21:16:05 +0100

    Refresh Python compatibility
    - Drop Python 3.3 and 3.4
    - Support Python 3.5 and 3.6

--- setup.py
+++ setup.py
@@ -49,8 +49,8 @@ setup_args = dict(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
 
     install_requires=requires,
