5721ae6aef0f60ee1363169f8ff37838b70144c2
Petr Viktorin <encukou@gmail.com>
2017-02-04 14:21:06 +0100

    Bump version to 0.3.5

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ if sys.version_info < (3, 4):
 
 setup_args = dict(
     name='pyvodb',
-    version='0.3.4',
+    version='0.3.5',
     packages=find_packages(),
     url='https://github.com/pyvec/pyvodb',
 
